65d0f6328df139d81289130f7b1521f5b9c556e4
AleksandrSkulinets <129200582+AleksandrSkulinets@users.noreply.github.com>
2023-03-28 20:01:13 +0300

    229

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool priceCorrect = false;
+
+            while(!priceCorrect)
+            {
+                Console.Write("hinta =");
+                int price = int.Parse(Console.ReadLine());
+                if (price <= 100 && price >= 0)
+                {
+                    Console.WriteLine($"price with discount= {price - (price * 0.1)}");
+                }
+                else if (price >= 100)
+                {
+                    Console.WriteLine($"price with discount= {price - (price * 0.25)}");
+                }
+                else 
+                    { Console.WriteLine("price is wrong");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+        }
+    }
+}
